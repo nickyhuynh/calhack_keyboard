8fb950a7ade4df6e8913fd825c08649c465256eb
Ayden Zhang <yitengzhang@airbears2-10-142-49-13.airbears2.1918.berkeley.edu>
2016-11-13 08:59:30 -0800

    fixes

--- touchpad.py
+++ touchpad.py
@@ -11,40 +11,63 @@ def merge(pos1, pos2):
     return ((pos1[0][0] + pos2[0][0]) / 2, (pos1[0][1] + pos2[0][1]) / 2), pos1[1] + pos2[1]
 
 
+def absolute_to_relative(val_x, val_y, x_axis, y_axis, angle):
+    """ shift and rotate position to fit the relative coordinate """
+    x = val_x - x_axis
+    y = val_y - y_axis
+    print(x, y)
+    return x * math.cos(angle) - y * math.sin(angle), x * math.sin(angle) + y * math.cos(angle)
+
+
 class Hand:
     """ each finger has structure ((center_x, center_y), size) """
     def __init__(self, palm):
         self.palm = palm
         self.fingers = []
         self.thumb = None
-        self.left = None  # whether this is the left hand
-        self.dir = 0.0
+        self.is_left = None  # whether this is the left hand
+        self.dir = (0.0, 0.0)
+        self.sep = 0.0
 
     def add_finger(self, finger):
         self.fingers.append(finger)
 
     def formulate(self):
-        # separates thumb from fingers and decides self.left
+        # separates thumb from fingers, decides self.left, self.dir and self.sep
         self.fingers = sorted(self.fingers, key=lambda x: x[1], reverse=True)
         self.thumb = self.fingers.pop(0)
+
         self.fingers = sorted(self.fingers, key=lambda x: distance2(x, self.thumb))
         f_1 = self.fingers[0]
         f_34 = merge(self.fingers[2], self.fingers[3])
         self.dir = float(f_34[0][1] - f_1[0][1]), float(f_34[0][0] - f_1[0][0])
         thumb_dir = float(self.thumb[0][1] - f_1[0][1]), float(self.thumb[0][0] - f_1[0][0])
         angle = math.atan2(thumb_dir[1], thumb_dir[0]) - math.atan2(self.dir[1], self.dir[0])
-        self.left = -math.pi < angle < 0 or angle > math.pi
+        self.is_left = -math.pi < angle < 0 or angle > math.pi
+
+        for i in range(len(self.fingers) - 1):
+            self.sep += math.sqrt(float(distance2(self.fingers[i], self.fingers[1 + i])))
+        self.sep = self.sep / 4.0 * 0.8
+
+    def get_key(self, pos):
+        angle = -math.atan2(self.dir[1], self.dir[0]) + (math.pi if self.is_left else 0)
+        x, y = absolute_to_relative(pos[0][0], -pos[0][1], self.fingers[0][0][0], -self.fingers[0][0][1], angle)
+        print(x, y)
+        if x >= self.sep * 1.5:
+            return 'Q'
+        elif x <= -self.sep * 1.5:
+            return 'Z'
+        else:
+            return 'A'
 
 
 class Touchpad:
     def __init__(self):
         self.empty_image = []
         self.current_image = None
-        self.current_map = None
         self.count = 0
         self.left = None
         self.right = None
-
 
     def process_image(self, image):
         self.current_image = image
@@ -52,8 +75,11 @@ class Touchpad:
             fingers = sorted(self.get_fingers(), key=lambda x: x[1], reverse=True)
             if len(fingers) >= 12:
                 self.separate_hands(fingers)
-            else:
-                pass
+            elif self.left and self.right:
+                for i in fingers:
+                    key1 = self.left.get_key(i)
+                    key2 = self.right.get_key(i)
+                    # print key1 if key1 else key2,
 
         else:
             self.empty_image.append(self.current_image)
@@ -176,7 +202,7 @@ class Touchpad:
             return
         hand1.formulate()
         hand2.formulate()
-        if hand1.left:
+        if hand1.is_left:
             self.left = hand1
             self.right = hand2
         else:
